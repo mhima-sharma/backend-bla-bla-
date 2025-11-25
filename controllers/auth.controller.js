e2bfe31414d2673562379026946dfa716befec9d
mahima-sharma <sharmamahima0510@gmail.com>
2025-11-25 16:55:17 +0530

    changes for render

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const db = require('../config/db')
