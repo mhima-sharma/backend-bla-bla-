9770e426d1a65d84a136824fc6dd4f7e18ecf57a
mahima-sharma <sharmamahima0510@gmail.com>
2025-11-26 10:24:30 +0530

    CREDIENTIAL

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
-  user: 'EXiquHJCwfBcpbR.root',
-  password: 'nF62NsJHp3nYcyFq',
-  database: 'buddy_ride',
+  host: process.env.DB_HOST || 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+  user: process.env.DB_USER || 'EXiquHJCwfBcpbR.root',
+  password: process.env.DB_PASS || 'nF62NsJHp3nYcyFq',
+  database: process.env.DB_NAME || 'buddy_ride',
   // waitForConnections: true,
   // connectionLimit: 10,
   // queueLimit: 0
