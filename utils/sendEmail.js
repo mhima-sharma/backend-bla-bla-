ded752fc00c278807929f781d8efa99fccc31315
mahima-sharma <sharmamahima0510@gmail.com>
2026-01-31 18:18:28 +0530

    mail pass and user

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.SMTP_EMAIL,
-    pass: process.env.SMTP_PASSWORD
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 });
 
 module.exports = async function sendEmail(to, subject, text) {
   const mailOptions = {
-    from: process.env.SMTP_EMAIL,
+    from: process.env.MAIL_USER,
     to,
     subject,
     text
